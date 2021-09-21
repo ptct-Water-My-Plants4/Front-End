9cb159ada5a8fdb8b149e239ad4260d3c910de25
Collin-Falstad <wildkard91@gmail.com>
2021-09-21 17:34:03 -0400

    connected user sign up form to the nav bar route

--- Plant Tracker/src/App.js
+++ Plant Tracker/src/App.js
@@ -9,6 +9,7 @@ import { Route, Link, Switch } from "react-router-dom";
 import axios from "axios";
 import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
+import Signup from "./components/Signup";
 
 
 function App() {
@@ -33,7 +34,7 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/login">Log In</Link>
-          <Link to="/register">Sign Up</Link>
+          <Link to="/signup">Sign Up</Link>
           <Link to="/" onClick={logout}>
             Log Out
           </Link>
@@ -45,7 +46,10 @@ function App() {
         <Route path="/login">
           <Login />
         </Route>
-        <Route path="/register">
+        <Route path="/signup">
+          <Signup />
+          </Route>
+        <Route path="/signup">
         </Route>
       </Switch>
     </div>
